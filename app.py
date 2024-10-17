c29e04bde66620da6668ff28534099d84af8fe3f
Nuthan B <156648630+Techno-Furious@users.noreply.github.com>
2024-10-17 22:16:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("My Google Form Submitter")
 number_of_submissions=st.number_input("Number of submissions", min_value=1, value=1)
 emails=st.text_area("email", "Enter emails")
 
-if st.button("Spam"):
+if st.button("JUST DO IT"):
     spam(number_of_submissions, emails)
     st.write(f"Completed {number_of_submissions} submissions")
